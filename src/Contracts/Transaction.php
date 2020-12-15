e340a2f0dfdd09eb9b044472db36b9883a7a0570
Xuan Yang <xuan.yang@goldenf.co>
2020-12-15 10:33:20 +0800

    contract init

---
+++ src/Contracts/Transaction.php
@@ -0,0 +1,103 @@
+<?php
+declare(strict_types=1);
+namespace GiocoPlay\Prism\Constracts\Operator;
+/**
+ * 交易
+ */
+interface Transaction {
+    /**
+     * 上分
+     * @param string $account_with_op
+     * @param string $wallet_code
+     * @param float $amount
+     * @param string $trace_id
+     * @return array
+     */
+    function opTransferIn(string $account_with_op, string $wallet_code, float $amount, string $trace_id): array;
+
+    /**
+     * 下分
+     * @param string $account_with_op
+     * @param string $wallet_code
+     * @param float $amount
+     * @param string $trace_id
+     * @return array
+     */
+    function opTransferOut(string $account_with_op, string $wallet_code, float $amount, string $trace_id): array;
+
+    /**
+     * 遊戲上分
+     * @param string $account_with_op
+     * @param string $wallet_code
+     * @param float $amount
+     * @param string $trace_id
+     * @return array
+     */
+    function gameTransferIn(string $account_with_op, string $wallet_code, float $amount, string $trace_id): array;
+
+    /**
+     * 遊戲下分
+     * @param string $account_with_op
+     * @param string $wallet_code
+     * @param float $amount
+     * @param string $trace_id
+     * @return array
+     */
+    function gameTransferOut(string $account_with_op, string $wallet_code, float $amount, string $trace_id): array;
+
+    /**
+     * 下注
+     * @param string $account_with_op
+     * @param string $wallet_code
+     * @param float $amount
+     * @param string $trace_id
+     * @param string $bet_id
+     * @return array
+     */
+    function gameStake(string $account_with_op, string $wallet_code, float $amount, string $trace_id, string $bet_id): array;
+
+    /**
+     * 派彩
+     * @param string $account_with_op
+     * @param string $wallet_code
+     * @param float $amount
+     * @param string $trace_id
+     * @param string $bet_id
+     * @return array
+     */
+    function gamePayoff(string $account_with_op, string $wallet_code, float $amount, string $trace_id, string $bet_id): array;
+
+    /**
+     * 取消下注
+     * @param string $account_with_op
+     * @param string $wallet_code
+     * @param float $amount
+     * @param string $trace_id
+     * @param string $bet_id
+     * @return array
+     */
+    function cancelStake(string $account_with_op, string $wallet_code, float $amount, string $trace_id, string $bet_id): array;
+
+    /**
+     * 取消派彩
+     * @param string $account_with_op
+     * @param string $wallet_code
+     * @param float $amount
+     * @param string $trace_id
+     * @param string $bet_id
+     * @return array
+     */
+    function cancelPayoff(string $account_with_op, string $wallet_code, float $amount, string $trace_id, string $bet_id): array;
+
+    /**
+     * 調整資金
+     * @param string $account_with_op
+     * @param string $wallet_code
+     * @param float $amount
+     * @param string $trace_id
+     * @param string $memo
+     * @return array
+     */
+    function adjust(string $account_with_op, string $wallet_code, float $amount, string $trace_id, string $memo): array;
+}
+
